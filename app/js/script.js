fc5ee31065d09da51ab988df6af00455662b6ce4
Andres Soria <andres.zov@gmail.com>
2023-01-22 13:09:32 -0600

    Add styles to show nav and menu but don't allow scroll

--- app/js/script.js
+++ app/js/script.js
@@ -1,16 +1,19 @@
 const btnHamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const fadeEls = document.querySelectorAll(".has-fade");
 
 btnHamburger.addEventListener("click", () => {
   if (header.classList.contains("open")) {
+    body.classList.remove("noscroll");
     header.classList.remove("open");
     fadeEls.forEach((el) => {
       el.classList.remove("fade-in");
       el.classList.add("fade-out");
     });
   } else {
+    body.classList.add("noscroll");
     header.classList.add("open");
     fadeEls.forEach((el) => {
       el.classList.remove("fade-out");
